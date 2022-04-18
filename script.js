ca443794cf5e27db5107e48342a64285f91dccf3
Serhii <igorevich808@gmail.com>
2022-04-18 15:28:54 +0300

    First exercise

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?');
+const personalMovieDB = {
+   count : numberOfFilms,
+   movies : {},
+   actors : {},
+   genres : [],
+   private : false,
+}
 
-var result = confirm ('Ghhh');
-console.log(result);
+const lastView = prompt ('Один из последних просмотренных фильмов?');
+const grade = +prompt ('На сколько оцените его?');
+
+personalMovieDB.movies[lastView] = grade;
+
+console.log(personalMovieDB);
+
 
