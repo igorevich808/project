509c986060fff96087230ff20cf2e575da3881d0
Serhii <igorevich808@gmail.com>
2022-04-19 21:23:28 +0300

    function

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?');
+let numberOfFilms;
+function start() {
+   numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?');
+   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+   numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?');
+   }
+}
+start();
 
 const personalMovieDB = {
    count : numberOfFilms,
@@ -8,34 +15,52 @@ const personalMovieDB = {
    private : false
 };
 
-if (personalMovieDB.count < 10) {
-   alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count > 9 && personalMovieDB.count < 30) {
-   alert('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-   alert('Вы киноман');
-} else {
-   alert('Произошла ошибка');
+function detectPersonalLevel() {
+   if (personalMovieDB.count < 10) {
+      alert('Просмотрено довольно мало фильмов');
+   } else if (personalMovieDB.count > 9 && personalMovieDB.count < 30) {
+      alert('Вы классический зритель');
+   } else if (personalMovieDB.count >= 30) {
+      alert('Вы киноман');
+   } else {
+      alert('Произошла ошибка');
+   } 
+}
+detectPersonalLevel();
+
+function rememberMyFilms() {
+   let lastView, grade;
+   for (let i = 0; i < 3; i++) {
+      lastView = prompt ('Один из последних просмотренных фильмов?');
+      if (lastView == '' || lastView == null || lastView.length > 50) {
+         alert ('Некорректное значение. Введите еще раз!');
+         i--;
+         continue;
+      }
+      grade = +prompt ('На сколько оцените его?');
+      if (grade == '' || lastView == null) {
+         alert ('Некорректное значение. Введите еще раз!');
+         i--;
+         continue;
+      }
+      personalMovieDB.movies[lastView] = grade;
+   }
+}
+//rememberMyFilms ();
+
+function showMyDB(hidden) {
+   if (!hidden) {
+      console.log(personalMovieDB);
+   } 
 }
 
-let lastView, grade;
-for (let i = 0; i < 3; i++) {
-   lastView = prompt ('Один из последних просмотренных фильмов?');
-   if (lastView == '' || lastView == null || lastView.length > 50) {
-      alert ('Некорректное значение. Введите еще раз!');
-      i--;
-      continue;
+function writeYourGenres(genres) {
+   for (let i = 0; i < 3; i++) {
+      genres[i] = prompt (`Ваш любимый жанр под номером ${i + 1}`);
    }
-   grade = +prompt ('На сколько оцените его?');
-   if (grade == '' || lastView == null) {
-      alert ('Некорректное значение. Введите еще раз!');
-      i--;
-      continue;
-   }
-   personalMovieDB.movies[lastView] = grade;
 }
+writeYourGenres(personalMovieDB.genres);
+showMyDB(personalMovieDB.private);
 
-
-console.log(personalMovieDB);
 
 
